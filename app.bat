855802875e8bcf95f667dfe1127685841d1d3ebe
nrishabh <rishabh.nanawati7@gmail.com>
2022-09-27 13:31:25 +0530

    reset_v3

--- app.bat
+++
@@ -1,4 +0,0 @@
-call C:\Users\Rishabh\anaconda3\Scripts\activate.bat
-call conda activate sweety
-python script.py
-pause
--- old_stuff/src/scripts/main.py
+++
@@ -1,8 +0,0 @@
-import os
-import project
-import project_support
-import functions
-
-if __name__ == '__main__':
-    if not os.path.exists(r"logs/"):
-        os.makedirs(r"logs/")
